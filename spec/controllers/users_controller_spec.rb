4495a61a0ebbe1383beddf505c7e9f6a59b32087
EpicMan2 <YOUR@EMAIL.com>
2024-02-12 20:34:01 +0200

    Fixed tests for not clearing base after tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe UsersController, type: :controller do
   describe 'POST #create' do
     context 'with valid parameters' do
       it 'creates a new user' do
-        post :create, params: { user: { email: 'test@example.com', password: 'password', password_confirmation: 'password' } }
+        post :create, params: { user: { email: 'working@example.com', password: 'password', password_confirmation: 'password' } }
         expect(response).to have_http_status(:created)
       end
     end
 
     context 'with invalid parameters' do
       it 'does not create a new user' do
-        post :create, params: { user: { email: 'invalid_email@example.com', password: 'password', password_confirmation: 'password' } }
+        post :create, params: { user: { email: 'working@example.com', password: '', password_confirmation: '' } }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,11 +2,25 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'rspec/json_expectations'
+require 'database_cleaner/active_record'
+require 'json_spec'
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
-end
 
-require 'json_spec'
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation
+    DatabaseCleaner.strategy = :transaction
+    Rails.application.load_seed
+  end
+  
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
 
 
